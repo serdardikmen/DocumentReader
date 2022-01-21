00897ee0978af285ddbd49b17b79fee8346bf59d
serdardikmen <60195944+serdardikmen@users.noreply.github.com>
2022-01-21 14:20:46 +0300

    iyileştirme.

--- Document_Reader/MainWindow.cs
+++ Document_Reader/MainWindow.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Data;
@@ -15,6 +16,16 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
 
 namespace Document_Reader
 {
@@ -26,33 +37,51 @@ namespace Document_Reader
     public MainWindow()
     {
       InitializeComponent();
-      ReadCsv(@"C:\Users\serda\Desktop\Analizör CSV verileri\Analizör CSV verileri\XX.XX.XX-XX.XX-TemelveHarmonik.csv");
-     
-    }
+      //ReadCsv(@"C:\Users\serda\Desktop\Analizör CSV verileri\Analizör CSV verileri\XX.XX.XX-XX.XX-TemelveHarmonik.csv");
 
+    }
+    
     private string[] lines;
     public void ReadCsv(string file)
     {
       lines = File.ReadAllLines(file);
       ReadColumns();
       ReadRows();
+
+    }
+
+    private void btn_start(object sender, RoutedEventArgs e)
+    {
+      OpenFileDialog file = new OpenFileDialog();
+      file.Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*";
+      file.FilterIndex = 1;
+      file.RestoreDirectory = true;
+      file.CheckFileExists = false;
+      file.Title = "Aktarım Yapılacak csv Dosyası Seçiniz..";
+      file.Multiselect = false;
+      file.ShowDialog();
+      ReadCsv(file.FileName);
+      DataTable dt = new DataTable();
+      dt.Columns.Add();
+
     }
     public void ReadColumns()
     {
       string[] columnlines = lines[0].Split(";");
-      for (int i = 0; i < columnlines.Length; i++) 
+      for (int i = 0; i < columnlines.Length; i++)
       {
-          DG.Columns.Add(new DataGridTextColumn()
-          {
-           Header = columnlines[i],
-           Binding = new Binding(String.Format("[{0}]",i))
-          });
+        DG.Columns.Add(new DataGridTextColumn()
+        {
+          Header = columnlines[i],
+          Binding = new Binding(String.Format("[{0}]", i))
+
+        });
       }
     }
-   private void ReadRows()
+    private void ReadRows()
     {
       DataTable dt = new DataTable();
-      for (int i = 1; i<lines.Length; i++)
+      for (int i = 1; i < lines.Length; i++)
       {
         DG.Items.Add(lines[i].Split(";"));
         //foreach (var l in line.Split(";"))
@@ -60,11 +89,9 @@ namespace Document_Reader
         //  //DG.Items.Add(l);
         //  dt.Rows.Add("abc");
         //}
-
       }
-      //DG.ItemsSource = dt.;
     }
   }
 }
-   
-     
+//DG.ItemsSource = dt.;
+
---
+++ Document_Reader/Sections.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+
+namespace DocumentReader
+{
+  public partial class SectionsExample : UserControl
+  {
+    public SectionsExample()
+    {
+      InitializeComponent();
+      DataTable dt = new DataTable();
+      SeriesCollection = new SeriesCollection
+      
+     
+            {
+                new LineSeries
+                {
+                    Values = new ChartValues<ObservableValue>
+                    {
+                        new ObservableValue(3),
+                        new ObservableValue(5),
+                        new ObservableValue(2),
+                        new ObservableValue(7),
+                        new ObservableValue(7),
+                        new ObservableValue(4)
+                    },
+                    PointGeometrySize = 0,
+                    StrokeThickness = 4,
+                    Fill = Brushes.Transparent
+                },
+                new LineSeries
+                {
+                    Values = new ChartValues<ObservableValue>
+                    {
+                        new ObservableValue(3),
+                        new ObservableValue(4),
+                        new ObservableValue(6),
+                        new ObservableValue(8),
+                        new ObservableValue(7),
+                        new ObservableValue(5)
+                    },
+                    PointGeometrySize = 0,
+                    StrokeThickness = 4,
+                    Fill = Brushes.Transparent
+                }
+            };
+
+
+      dt.Columns.Add();
+    }
+
+    private void InitializeComponent()
+    {
+      throw new NotImplementedException();
+    }
+
+    public SeriesCollection SeriesCollection { get; set; }
+
+    private void UpdateAllOnClick(object sender, RoutedEventArgs e)
+    {
+      var r = new Random();
+
+      foreach (var series in SeriesCollection)
+      {
+        foreach (var observable in series.Values.Cast<ObservableValue>())
+        {
+          observable.Value = r.Next(0, 10);
+        }
+      }
+    }
+  }
+}
